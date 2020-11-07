79a445652b874117f7185cebaf3f35827f22e877
Rohan Agarwal <ro.agarwal@hotmail.com>
2020-11-07 13:10:49 -0500

    Add entity count

--- textanalysis.py
+++ textanalysis.py
@@ -1,16 +1,19 @@
 import os
 import csv
 import json
+from dotenv import load_dotenv
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions, EntitiesOptions
 
-authenticator = IAMAuthenticator('k4sZVsUjG-ojC-nkkh1TCS0PzrQh9TAo1v6NNQVsqVEW')
+load_dotenv()
+IBM_CLOUD_KEY = os.getenv('IBM_CLOUD_KEY')
+
+authenticator = IAMAuthenticator(IBM_CLOUD_KEY)
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2020-08-01',
     authenticator=authenticator
 )
-
 natural_language_understanding.set_service_url('https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/90774996-ec17-4440-b524-2c61f3a14481')
 
 path = "C:\Learning\HackRPI2020\HackRPI2020\data"
@@ -21,7 +24,7 @@ with open(path, 'r') as csvinput, open(path, 'w') as csvoutput:
     all = []
     row = next(reader)
     row.append('Sentiment')
-    row.append('Embedding')
+    row.append('Entity_Num')
     row.append('Word_Count')
     all.append(row)
 
@@ -31,7 +34,7 @@ with open(path, 'r') as csvinput, open(path, 'w') as csvoutput:
         # get analysis
         sentiment = 0.0
         magnitude = 0.0
-        embedding = 0.0
+        entity_num = 0.0
         word_count = len(text.split())
 
         # sentiment analysis
@@ -46,12 +49,13 @@ with open(path, 'r') as csvinput, open(path, 'w') as csvoutput:
         sentiment_sum = 0
         for entity in entities_response['entities']:
             sentiment_sum += entity['sentiment']['score']
+            entity_num += 1
 
         sentiment = sentiment + sentiment_sum / 2
 
         # append that analysis
         row.append(sentiment)
-        row.append(embedding)
+        row.append(entity_num)
         row.append(word_count)
         all.append(row)
 
