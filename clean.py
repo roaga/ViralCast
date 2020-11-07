49ae7bbf6e5b3b12916966082295bdb1aa8d18d5
baotran181201 <quoc.bao.tran1218@gmail.com>
2020-11-07 23:33:05 +0700

    Clean data for 3 days

--- clean.py
+++ clean.py
@@ -1,9 +1,17 @@
 import pandas as pd
-df = pd.read_csv('2020-04-16 Coronavirus Tweets.csv')
+import glob
+path = r'C:\Learning\HackRPI2020\HackRPI2020\data'
+all_files = glob.glob(path + "/*.csv")
+li = []
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    li.append(df)
+frame = pd.concat(li, axis=0, ignore_index=True)
+
 cols_to_drop = ['status_id', 'user_id', 'created_at','screen_name','source','reply_to_status_id'
 ,'reply_to_user_id','reply_to_screen_name','is_quote','is_retweet','country_code','place_full_name',
 'place_type','friends_count','account_lang','account_created_at','verified','lang']
 
 for col in cols_to_drop:
-    del df[col]
-print(df.shape)
+    del frame[col]
+print(frame.shape)
