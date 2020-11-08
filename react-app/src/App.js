7ca5d68cae3f14e970c8c74c8ec82bd5b630a4ab
Rohan Agarwal <ro.agarwal@hotmail.com>
2020-11-08 11:24:53 -0500

    Rebuild

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -22,13 +22,12 @@ function App() {
     const handleTweet = (e) => {
         e.preventDefault();
         // extract features
-        let hostname = window.location.hostname;    
-        fetch("https://" + hostname + '/api/features/' + tweet.replace("#", "").replace(" ", "-").replace("?", "").replace("/", "") + "/" + followers + "/" + friends + "/" + verified).then(res => res.json()).then(
+        fetch('https://viral-cast.herokuapp.com/api/features/' + tweet.replace("#", "").replace(" ", "-").replace("?", "").replace("/", "") + "/" + followers + "/" + friends + "/" + verified).then(res => res.json()).then(
             (result) => {
                 setFeatures(result)
 
                 // pass features into ML model
-                fetch("https://" + hostname + '/api/predict/' + JSON.stringify(result).replace(" ", "-")).then(res => res.json()).then(
+                fetch('https://viral-cast.herokuapp.com/api/predict/' + JSON.stringify(result).replace(" ", "-")).then(res => res.json()).then(
                     (result) => {
                         setFavorites(Math.round(Math.abs(result['favorites']) / 20))
                         setRetweets(Math.round(Math.abs(result['retweets'])))
