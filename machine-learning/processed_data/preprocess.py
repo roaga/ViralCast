1788d6329b8031ada25a88447e4ba93086c70484
Jed <jedmagracia@gmail.com>
2020-11-07 22:13:22 -0500

    Updated model

--- machine-learning/processed_data/preprocess.py
+++ machine-learning/processed_data/preprocess.py
@@ -7,14 +7,14 @@ import csv
 path = os.path.join(os.getcwd(), "cleaned_tweets.csv")
 
 with open(path, 'r') as csvinput:
-    with open(os.path.join(os.getcwd(), "data.csv"), 'w') as csvoutput:
+    with open(os.path.join(os.getcwd(), "data.csv"), 'a') as csvoutput:
         rows = csv.reader(csvinput)
         writer = csv.writer(csvoutput, lineterminator='\n')
-        next(rows)
+        # next(rows)
 
-        header = ["sentiment", "entity_num", "word_count",
-                  "avg_word_len", "char_count", "anger", "disgust", "fear", "joy", 
-                  "sadness", "is_quote", "favorites", "retweets", "followers", 
-                  "friends", "verified"]
-        writer.writerow(header)
+        # header = ["sentiment", "entity_num", "word_count",
+        #           "avg_word_len", "char_count", "anger", "disgust", "fear", "joy", 
+        #           "sadness", "is_quote", "followers", "friends", "verified",
+        #           "retweets", "favorites"]
+
         textanalysis.process_rows(rows, writer)
