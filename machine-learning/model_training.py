9f3c4fd6b1a01397b50903f38ecc00893fb25856
Jed <jedmagracia@gmail.com>
2020-11-07 23:04:11 -0500

    Added mock models

--- machine-learning/model_training.py
+++ machine-learning/model_training.py
@@ -1,6 +1,7 @@
-from sklearn.linear_model import Ridge, LinearRegression
+from sklearn.linear_model import *
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn import metrics
 from matplotlib import pyplot as plt
 import numpy as np
 import csv
@@ -11,7 +12,7 @@ import os
 """
 Pre-process the data here
 """
-path = os.path.join(os.getcwd(), "processed_data", "data.csv")
+path = os.path.join(os.getcwd(), "processed_data", "less_features.csv")
 with open(path, 'r') as csv_data:
     next(csv_data)
     data = np.loadtxt(csv_data, delimiter=',')
@@ -32,31 +33,25 @@ print(features.shape)
 
 # Splitting train data and test data
 # Retweets Data Split
-retweets_features_train, retweets_features_test, retweets_train, retweets_test = train_test_split(
-    features, retweets, test_size=0.2, shuffle=True
-    )
+retweets_features_train, retweets_features_test, retweets_train, retweets_test = train_test_split(features, retweets)
 
 # Favorites Data Split
-favorites_features_train, favorites_features_test, favorites_train, favorites_test = train_test_split(
-    features, favorites, test_size=0.2, shuffle=True
-)
-
+favorites_features_train, favorites_features_test, favorites_train, favorites_test = train_test_split(features, favorites)
  # Algorithm 
-retweet_linear_regression = LinearRegression(normalize=True)
-favorites_linear_regression = LinearRegression(normalize=True)
+retweet_linear_regression = Ridge(max_iter=10000)
+favorites_linear_regression = Ridge(max_iter=10000)
 
 retweet_linear_regression.fit(retweets_features_train, retweets_train)
 favorites_linear_regression.fit(favorites_features_train, favorites_train)
 
 retweets_pred = retweet_linear_regression.predict(retweets_features_test)
 favorites_pred = favorites_linear_regression.predict(favorites_features_test)
-print(retweets_pred - retweets_test)
-print(favorites_pred - retweets_test)
 
-retweet_dump = joblib.dump(retweet_linear_regression, "/models/retweet_model.joblib,pkl", compress=9)
-favorite_dump = joblib.dump(favorites_linear_regression, "/models/favorites_model.joblib,pkl", compress=9)
+r_square_retweets = metrics.r2_score(retweets_test, retweets_pred)
+r_square_favorites = metrics.r2_score(favorites_test, favorites_pred)
+print(r_square_retweets)
+print(r_square_favorites)
 
-retweets_score = retweet_linear_regression.score(retweets_features_test, retweets_test)
-favorites_score = favorites_linear_regression.score(favorites_features_test, favorites_test)
-# print("Retweets Score: " + str(retweets_score))
-# print("Favorites Score: " + str(favorites_score))
+retweet_dump = joblib.dump(retweet_linear_regression, "models/retweet_model.joblib,pkl", compress=9)
+favorite_dump = joblib.dump(favorites_linear_regression, "models/favorites_model.joblib,pkl", compress=9)
+
